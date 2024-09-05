a909de5c2056e99a81b5b47af273b5d7a50c8411
Jay Bird <jubilates@hotmail.com>
2024-09-05 16:14:41 +0200

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/barcollin/TLS-in-go
 
 go 1.22.2
+
+require github.com/golang/mock v1.6.0 // indirect
---
+++ pkg/cert/x509.go
@@ -0,0 +1,9 @@
+package cert
+
+func CreateCACert(ca *CACert, keyFilePath, caCertFilePath string) error {
+	return nil
+}
+
+func CreateCert(cert *Cert, caKey []byte, keFilePath, certFilePath string) error {
+	return nil
+}
