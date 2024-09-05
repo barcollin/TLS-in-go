1a2e0dd6b8de05b38b45e8d793a438f8853321aa
Jay Bird <jubilates@hotmail.com>
2024-09-05 16:32:37 +0200

    feat: add the root cobra command for tls

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -1,5 +1,25 @@
 package cmd
 
-func Execute() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "tls",
+	Short: "tls is a command line tool for TLS",
+	Long: `A Fast and Flexible Static Site Generator built 
+				with love by spf13 and friends in Go.`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
